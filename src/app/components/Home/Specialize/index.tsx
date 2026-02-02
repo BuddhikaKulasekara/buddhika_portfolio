62773caa908e6dafac04b286cc95eb721ef00dc7
BuddhikaKulasekara <kulasekara797@gmail.com>
2026-02-02 22:15:58 +0530

    feat: Expertise change

--- src/app/components/Home/Specialize/index.tsx
+++ src/app/components/Home/Specialize/index.tsx
@@ -29,7 +29,7 @@ const Specialize = () => {
     <section id='expertise' className='scroll-mt-12'>
       <div className='container'>
         <div className='text-center mb-8'>
-          <h2 className='mb-6'>Our Expertise</h2>
+          <h2 className='mb-6'>My Expertise</h2>
           <p className='text-lg font-normal max-w-2xl mx-auto'>
             Our team crafts creative strategies that elevate your brand, engage
             your audience, and drive results.
@@ -39,28 +39,28 @@ const Specialize = () => {
         <div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6'>
           {loading
             ? Array.from({ length: 6 }).map((_, i) => (
-                <SpecializeSkeleton key={i} />
-              ))
+              <SpecializeSkeleton key={i} />
+            ))
             : specialization.map((item, i) => (
-                <div key={i}>
-                  <div className='bg-secondary dark:bg-darklight rounded-lg p-8'>
-                    <div className='p-3 rounded-lg bg-primary w-fit mb-8 shadow-lg shadow-primary/30'>
-                      <Image
-                        src={item.imgSrc}
-                        alt={item.title}
-                        width={24}
-                        height={24}
-                      />
-                    </div>
-                    <div>
-                      <h5 className='font-bold mb-2'>{item.title}</h5>
-                      <p className='text-base font-normal max-w-xs'>
-                        {item.desc}
-                      </p>
-                    </div>
+              <div key={i}>
+                <div className='bg-secondary dark:bg-darklight rounded-lg p-8'>
+                  <div className='p-3 rounded-lg bg-primary w-fit mb-8 shadow-lg shadow-primary/30'>
+                    <Image
+                      src={item.imgSrc}
+                      alt={item.title}
+                      width={24}
+                      height={24}
+                    />
+                  </div>
+                  <div>
+                    <h5 className='font-bold mb-2'>{item.title}</h5>
+                    <p className='text-base font-normal max-w-xs'>
+                      {item.desc}
+                    </p>
                   </div>
                 </div>
-              ))}
+              </div>
+            ))}
         </div>
       </div>
     </section>
